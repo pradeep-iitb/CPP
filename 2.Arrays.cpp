0e7c17cb69c72d03b19d35fc35c77e81122950d1
Pradeep Kumawat <pradeepkumawat132005@gmail.com>
2025-11-12 11:33:32 +0000

    Learned Arrays

--- 2.Arrays.cpp
+++ 2.Arrays.cpp
@@ -1,7 +1,20 @@
 #include <iostream>
+#include <climits>
+#include <cstdlib>
 using namespace std;
 
 int main() {
-    cout << "Hello, World!" << endl;
+    int marks[] = {23, 45, 12, 67, 34};
+    int size = sizeof(marks) / sizeof(marks[0]);
+    int bigNum = 0;
+    for (int i = 0; i < size; i++) {
+        bigNum= (bigNum+marks[i]+(abs(marks[i]-bigNum)))/2;
+    }
+    cout<<"Largest Number: "<<bigNum<<endl;
+    int smallNum = INT_MAX ;
+    for (int i = 0; i < size; i++) {
+        smallNum= (smallNum+marks[i]-(abs(marks[i]-smallNum)))/2;
+    }
+    cout<<"Smallest Number: "<<smallNum<<endl;
     return 0;
 }
